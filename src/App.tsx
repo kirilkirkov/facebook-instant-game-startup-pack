4cbc718eb001e29b99476051a2c236c5b87c908b
Kiril Kirkov <kiro_tyson@abv.bg>
2022-12-20 09:14:57 +0200

    Add obtining of user info before start game

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/svg/react.svg'
 
 function App() {
+
+  const [HasFBUser, setHasFBUser] = useState(false)
+
+  useEffect(() => {
+    (async() => {
+        while(!window.hasOwnProperty("playerName")) // define the condition as you like
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            setHasFBUser(true)
+    })();
+  })
+
   return (
-    <div className="App">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-      Hello Instant Game!
+    <div>
+      <div className="App" style={{
+        display: HasFBUser ? "flex" : "none",
+      }}>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        Hello Instant {(window as any).playerName}!
+      </div>
+
+      {/* Wait for user info */}
+      <div style={{
+        display: !HasFBUser ? "block" : "none",
+      }}>
+        Getting your data
+      </div>
     </div>
   )
 }
